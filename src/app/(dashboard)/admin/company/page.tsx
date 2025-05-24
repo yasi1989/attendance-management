f6c6977169c063ef1357b334988170ad59688642
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-05-24 20:12:57 +0900

    fix:サイドバー修正

--- src/app/(dashboard)/admin/company/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateCompanyCodeForm from '@/features/admin/company/components/CreateCompanyCodeForm';
-import { fetchCompanyCode } from '@/features/admin/company/services/fetcnCompanyCode';
-
-const page = () => {
-  const companyCodes = fetchCompanyCode();
-  return <CreateCompanyCodeForm data={companyCodes} />;
-};
-
-export default page;
--- src/features/dashboard/const/sideMenuItems.ts
+++ src/features/dashboard/const/sideMenuItems.ts
@@ -1,4 +1,4 @@
-import { Calendar, UserCheck, UsersRound } from 'lucide-react';
+import { CheckCircle, Clock, Settings, Shield } from 'lucide-react';
 
 export const data = {
   user: {
@@ -8,13 +8,13 @@ export const data = {
   },
   navMain: [
     {
-      title: 'ホーム',
+      title: '申請・届出',
       url: '#',
-      icon: Calendar,
+      icon: Clock,
       isActive: true,
       items: [
         {
-          title: '出退勤申請',
+          title: '勤怠カレンダー',
           url: '/calendar',
         },
         {
@@ -22,38 +22,57 @@ export const data = {
           url: '/expense/add',
         },
         {
-          title: '経費履歴',
+          title: '経費申請履歴',
           url: '/expense/history',
         },
       ],
     },
     {
-      title: '承認者',
+      title: '承認業務',
       url: '#',
-      icon: UserCheck,
+      icon: CheckCircle,
       items: [
         {
-          title: '承認申請',
-          url: '#',
+          title: '承認待ち一覧',
+          url: '/approval/pending',
         },
         {
           title: '承認履歴',
-          url: '#',
+          url: '/approval/history',
         },
       ],
     },
     {
-      title: '管理者',
+      title: '会社管理',
       url: '#',
-      icon: UsersRound,
+      icon: Shield,
+      items: [
+        {
+          title: '社員管理',
+          url: '/company-admin/employees',
+        },
+        {
+          title: '部署・役職管理',
+          url: '/company-admin/organization',
+        },
+        {
+          title: '休日カレンダー',
+          url: '/company-admin/holiday',
+        },
+      ],
+    },
+    {
+      title: 'システム管理',
+      url: '#',
+      icon: Settings,
       items: [
         {
           title: 'ユーザ管理',
-          url: '#',
+          url: '/system-admin/users',
         },
         {
-          title: '会社コード一覧',
-          url: '/admin/company',
+          title: '会社管理',
+          url: '/system-admin/company',
         },
       ],
     },
