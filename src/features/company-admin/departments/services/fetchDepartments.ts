6032613fc2d229461b1aede33dc668fd13ee2806
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-06-16 12:46:56 +0900

    fix:フィルター修正

--- src/features/company-admin/departments/services/fetchDepartments.ts
+++ src/features/company-admin/departments/services/fetchDepartments.ts
@@ -5,8 +5,8 @@ import { UserType } from '@/features/system-admin/users/type/userType';
 export const fetchDepartments = (): { department: DepartmentType[]; users: UserType[] } => {
   // 本番はauth.jsからユーザ情報を取得
   const userId = 'u3a2b3c4-5d6e-789f-a1b2-c3d4e5f67892';
-  const user = users.find((user) => user.id === userId);
-  const myCompanyUsers = users.filter((user) => user.companyId === user?.companyId);
-  const department = departments.filter((department) => department.companyId === user?.companyId);
+  const myUser = users.find((user) => user.id === userId);
+  const myCompanyUsers = users.filter((user) => user.companyId === myUser?.companyId);
+  const department = departments.filter((department) => department.companyId === myUser?.companyId);
   return { department, users: myCompanyUsers };
 };
