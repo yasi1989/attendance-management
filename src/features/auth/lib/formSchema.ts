582f513f2a53e79dbd08f0785cc81dc0c1861f2f
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-08-15 17:59:19 +0900

    auth_schema修正

--- src/features/auth/lib/formSchema.ts
+++ src/features/auth/lib/formSchema.ts
@@ -1,22 +1,27 @@
+import { VALIDATION_LIMITS } from '@/consts/validate';
 import { z } from 'zod';
 
 const BaseLoginSchema = z.object({
   email: z
+    .string()
     .email({
       message: '有効なメールアドレスを入力してください。',
     })
     .min(1, {
       message: 'メールアドレスは必須です。',
     })
-    .max(255, {
-      message: 'メールアドレスは255文字以内で入力してください。',
+    .max(VALIDATION_LIMITS.EMAIL_MAX_LENGTH, {
+      message: `${VALIDATION_LIMITS.EMAIL_MAX_LENGTH}文字以内で入力してください。`,
     }),
   password: z
     .string()
-    .min(8, {
-      message: 'パスワードは8文字以上で入力してください。',
+    .min(VALIDATION_LIMITS.MIN_PASSWORD_LENGTH, {
+      message: `${VALIDATION_LIMITS.MIN_PASSWORD_LENGTH}文字以上で入力してください。`,
     })
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/, {
+    .max(VALIDATION_LIMITS.PASSWORD_MAX_LENGTH, {
+      message: `${VALIDATION_LIMITS.PASSWORD_MAX_LENGTH}文字以内で入力してください。`,
+    })
+    .regex(VALIDATION_LIMITS.PASSWORD_REGEX, {
       message: 'パスワードには英小文字、英大文字、数字、特殊文字（!@#$%^&*）を含めてください。',
     }),
 });
@@ -25,10 +30,10 @@ export const SignInSchema = BaseLoginSchema;
 export const SignUpSchema = BaseLoginSchema.extend({
   name: z
     .string()
-    .min(3, {
-      message: '名前は3文字以上で入力してください。',
+    .min(VALIDATION_LIMITS.MIN_LENGTH, {
+      message: '名前は必須です。',
     })
-    .max(255, {
-      message: '名前は255文字以内で入力してください。',
+    .max(VALIDATION_LIMITS.NAME_MAX_LENGTH, {
+      message: `${VALIDATION_LIMITS.NAME_MAX_LENGTH}文字以内で入力してください。`,
     }),
 });
