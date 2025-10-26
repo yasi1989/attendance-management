586a842abd6f34373afd2e110d9129ec7f8a7adb
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-10-26 18:23:18 +0900

    fix:認証ボタンにローダー追加

--- src/features/auth/components/SocialLoginForm.tsx
+++ src/features/auth/components/SocialLoginForm.tsx
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from '@/components/ui/button';
-import { startTransition } from 'react';
+import { useTransition } from 'react';
 import { signIn } from 'next-auth/react';
 import { SOCIAL_PROVIDERS } from '@/consts/providers';
 import { URLS } from '@/consts/urls';
+import { Loader2 } from 'lucide-react';
 
 const SocialLoginForm = () => {
+  const [isPending, startTransition] = useTransition();
   return (
     <div className="grid grid-cols-2 gap-3">
       {SOCIAL_PROVIDERS.map((provider) => (
@@ -19,9 +21,10 @@ const SocialLoginForm = () => {
               await signIn(provider.id, { callbackUrl: URLS.ROOT });
             });
           }}
+          disabled={isPending}
         >
           <FontAwesomeIcon icon={provider.icon} className="mr-2 h-4 w-4" />
-          {provider.name}
+          {isPending ? <Loader2 className="animate-spin" /> : provider.name}
         </Button>
       ))}
     </div>
