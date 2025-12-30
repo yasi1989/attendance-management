cce84370a6dd4f3016aa4714818c7b2e7c2addbf
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-12-31 00:19:37 +0900

    fix:コンフリクト

--- src/features/system/companies/components/CompaniesListTable.tsx
+++ src/features/system/companies/components/CompaniesListTable.tsx
@@ -1,11 +1,6 @@
 import { DataTable } from '@/components/table/DataTable';
-<<<<<<< HEAD
-import { companyColumns } from './CompaniesColumns';
 import { Company } from '@/lib/actionTypes';
-=======
 import { companyColumns } from '../table/CompaniesColumns';
-import { CompanyType } from '../type/companyType';
->>>>>>> main
 
 type CompaniesListTableProps = {
   companies: Company[];
