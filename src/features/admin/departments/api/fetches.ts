2f23ee910544527994cefcd84e6e74cf24d44db5
yasi1989 <kei.kobayashi.works1989@gmail.com>
2026-02-02 23:53:53 +0900

    fix:検索処理の追加

---
+++ src/features/admin/departments/api/fetches.ts
@@ -0,0 +1,35 @@
+'use server';
+import { requireCompanyAdmin } from '@/features/auth/lib/authRoleUtils';
+import { db } from '@/lib/db/drizzle';
+import { FetchDepartmentsDataResponse } from '../type/fetchResultResponse';
+
+export const fetchDepartments = async (): Promise<FetchDepartmentsDataResponse> => {
+  try {
+    const { user } = await requireCompanyAdmin();
+    if (!user.companyId) {
+      throw new Error('Company ID が設定されていません');
+    }
+    const [myDepartments, myCompanyUsers] = await Promise.all([
+      db.query.departments.findMany({
+        where: (departments, { eq }) => eq(departments.companyId, user.companyId),
+        orderBy: (departments, { asc }) => [asc(departments.departmentName)],
+      }),
+      db.query.users.findMany({
+        where: (users, { eq }) => eq(users.companyId, user.companyId),
+        with: {
+          department: true,
+          role: true,
+        },
+        orderBy: (users, { asc }) => [asc(users.name)],
+      }),
+    ]);
+
+    return {
+      departments: myDepartments,
+      users: myCompanyUsers,
+    };
+  } catch (error) {
+    console.error('データ取得に失敗しました。', error);
+    throw error;
+  }
+};
