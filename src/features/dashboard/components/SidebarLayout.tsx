dc2ac4b605e5100c757be07c2e1736eaf592d8db
yasi1989 <kei.kobayashi.works1989@gmail.com>
2026-03-28 22:18:00 +0900

    merge

--- src/features/dashboard/components/SidebarLayout.tsx
+++ src/features/dashboard/components/SidebarLayout.tsx
@@ -2,17 +2,19 @@
 
 import dynamic from 'next/dynamic';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { Role } from '@/lib/actionTypes';
 
 const AppSidebarClient = dynamic(() => import('./AppSidebar').then((mod) => mod.AppSidebar), { ssr: false });
 
 type SidebarLayoutProps = {
   children: React.ReactNode;
+  userRole: Role;
 };
 
-export const SidebarLayout = ({ children }: SidebarLayoutProps) => {
+export const SidebarLayout = ({ children, userRole }: SidebarLayoutProps) => {
   return (
     <SidebarProvider>
-      <AppSidebarClient />
+      <AppSidebarClient userRole={userRole} />
       <SidebarInset className="m-0! p-0!">{children}</SidebarInset>
     </SidebarProvider>
   );
